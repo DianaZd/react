afb33aa875472a7fa29ac4f19c7d5baaa2fec992
DianaZd <dipetrovnadi@gmail.com>
2022-07-20 18:39:49 +0400

    first commit

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Button.css';
+
+const Button = () => {
+    return <button className="my-button">Click1</button>;
+};
+
+export default Button;
---
+++ src/components/ButtonProps/ButtonProps.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ButtonProps.css'
+
+const ButtonProps = () => {
+    return (
+        <button className='button'>
+            Click2
+        </button>
+    );
+};
+
+export default ButtonProps;
